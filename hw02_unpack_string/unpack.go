bb3a4141bde874ae9556f13672722c3641bcc233
yakov-work <yakov.gulyuta@yandex.ru>
2023-02-17 13:18:50 +0300

    продумать валидацию

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var b strings.Builder
+
+	unicode := utf8.RuneCountInString(input)
+	length := len(input)
+
+	for i := 0; i < len(input); i++ {
+
+		atoiInt, _ := strconv.Atoi(string(input[i]))
+		_, err := strconv.Atoi(string(input[0]))
+		if err == nil {
+			return "", ErrInvalidString
+		}
+		if atoiInt == 0 {
+			b.WriteString(string(input[i]))
+		} else {
+			b.WriteString(strings.Repeat(string(input[i-1]), atoiInt-1))
+		}
+		//fmt.Println(prev, atoiInt)
+		//if (prev % 2) == 0 {
+		//	b.WriteString(string(input[i]))
+		//}
+
+	}
+
+	result := b.String()
+	b.Reset()
+
+	fmt.Println(result, unicode, length)
+
+	//strings.Repeat
+	//strconv.Atoi
+	return result, nil
 }
