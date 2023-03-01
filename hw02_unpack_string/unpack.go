542bb6e1c361a7dc01a91ed26ef19eede2b198ae
yakov-work <yakov.gulyuta@yandex.ru>
2023-03-01 16:58:09 +0300

    распаковка

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,10 +2,8 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
-	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
@@ -13,34 +11,23 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(input string) (string, error) {
 	var b strings.Builder
 
-	unicode := utf8.RuneCountInString(input)
+	//unicode := utf8.RuneCountInString(input)
 	length := len(input)
 
-	for i := 0; i < len(input); i++ {
+	for i := 0; i < length; i++ {
 
-		atoiInt, _ := strconv.Atoi(string(input[i]))
-		_, err := strconv.Atoi(string(input[0]))
-		if err == nil {
-			return "", ErrInvalidString
-		}
-		if atoiInt == 0 {
+		atoiInt, err := strconv.Atoi(string(input[i]))
+
+		if err != nil {
 			b.WriteString(string(input[i]))
 		} else {
-			b.WriteString(strings.Repeat(string(input[i-1]), atoiInt-1))
+			if atoiInt != 0 {
+				b.WriteString(strings.Repeat(string(input[i-1]), atoiInt-1))
+			}
 		}
-		//fmt.Println(prev, atoiInt)
-		//if (prev % 2) == 0 {
-		//	b.WriteString(string(input[i]))
-		//}
-
 	}
 
 	result := b.String()
 	b.Reset()
-
-	fmt.Println(result, unicode, length)
-
-	//strings.Repeat
-	//strconv.Atoi
 	return result, nil
 }
